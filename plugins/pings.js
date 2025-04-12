2b2a524d5ef3f6c377873e8c9ded24f8abddba20
Manju362 <pathumchinthaka398@gmail.com>
2025-04-13 00:57:37 +0530

    Update pings.js

--- plugins/pings.js
+++ plugins/pings.js
@@ -1,13 +1,13 @@
-module.exports = {
-  pattern: "ping",
-  alias: [".ping"],
-  desc: "Check bot's ping",
-  category: "system",
-  react: "🏓",
-  function: async (conn, mek, m, { reply }) => {
-    let start = new Date().getTime();
-    await reply("Ping වෙමින්...");
-    let end = new Date().getTime();
-    reply(`*MANJU_MD*\n_Response Time:_ ${end - start}ms`);
+const { cmd } = require("../command");
+
+cmd(
+  {
+    pattern: "ping",
+    desc: "Ping pong check",
+    category: "info",
+    filename: __filename,
   },
-};
+  async ({ msg }) => {
+    await msg.reply("Pong!");
+  }
+);
