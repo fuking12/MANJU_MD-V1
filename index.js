e917fc3409864221ce308b5ff9808ff2e3670e54
Manju362 <pathumchinthaka398@gmail.com>
2025-04-12 17:29:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -240,7 +240,7 @@ async function connectToWA() {
     //owener react
     if (senderNumber.includes("94766863255")) {
       if (isReact) return;
-      .react("🌝");
+      message.react("😃");
     }
 
     //work type
