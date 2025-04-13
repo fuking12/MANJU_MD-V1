9f6ace1d3c545a6823e6d0f8bc9d433d339cdedd
Manju362 <pathumchinthaka398@gmail.com>
2025-04-13 11:51:09 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,17 +1,19 @@
-module.exports = {
-  name: 'ping',
-  alias: ['speed', 'pong'],
-  description: 'Bot එකේ ප්‍රතිචාර වේගය පෙන්වයි',
-  category: 'info',
-  async execute(m, { conn }) {
-    const start = Date.now();
-    const msg = await m.reply('පරීක්ෂා කරමින්...');
-    const end = Date.now();
-    const ping = end - start;
+const { cmd } = require('../command');
 
-    await conn.sendMessage(m.chat, {
-      text: `🏓 *Ping ප්‍රතිචාර වේගය:* ${ping} ms`,
-      edit: msg.key
-    });
-  }
-};
+cmd({
+  name: 'ping',
+  alias: ['speed'],
+  category: 'info',
+  desc: 'Bot response speed test',
+  filename: __filename
+}, async (m, { conn }) => {
+  const start = Date.now();
+  const msg = await m.reply('පරීක්ෂා කරමින්...');
+  const end = Date.now();
+  const ping = end - start;
+
+  await conn.sendMessage(m.chat, {
+    text: `🏓 *Ping ප්‍රතිචාර වේගය:* ${ping} ms`,
+    edit: msg.key
+  });
+});
