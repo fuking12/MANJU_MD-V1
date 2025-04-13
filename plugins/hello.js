84776027787bde1818c09233d0b5dd1d0a715e2d
Manju362 <pathumchinthaka398@gmail.com>
2025-04-13 12:29:20 +0530

    Update hello.js

--- plugins/hello.js
+++ plugins/hello.js
@@ -6,5 +6,6 @@ cmd({
   category: "test",
   react: "🙏"
 }, async (client, m, sock) => {
+  console.log(Object.keys(sock)); // Print all methods and properties of 'sock' object
   await sock.sendMessage(m.chat, { text: "ආයුබෝවන්! ඔබට කොහොම ද?" }, { quoted: m });
 });
