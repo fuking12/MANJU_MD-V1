b87ac78fcbd534b5aec79246ee6bb74f651afe42
Manju362 <pathumchinthaka398@gmail.com>
2025-04-19 22:54:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -13,5 +13,5 @@ module.exports = {
   ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Manju362/Link-gamu./refs/heads/main/IMG-20250417-WA0196.jpg",
   ALIVE_MSG: process.env.ALIVE_MSG || "𝗛𝗜,𝗜,𝗔𝗠 𝗔𝗟𝗜𝗩𝗘 𝗡𝗢𝗪\n\n>𝗬𝗢𝗨 𝗖𝗔𝗡 𝗚𝗘𝗧 𝗔𝗟𝗟 𝗧𝗛𝗘 𝗠𝗢𝗩𝗜𝗘𝗦\n\n>𝗣𝗛𝗢𝗧𝗢𝗦, 𝗦𝗢𝗡𝗚𝗦,𝗘𝗗𝗨𝗖𝗔𝗧𝗜𝗢𝗡𝗔𝗟𝗦 𝗬𝗢𝗨 𝗪𝗔𝗡𝗧\n\n>𝗔𝗹𝗹 𝗧𝗛𝗥𝗢𝗨𝗚𝗛 𝗧𝗛𝗜𝗦 𝗕𝗢𝗧\n\n>𝗝𝗢𝗜𝗡 𝗠𝗬 𝗪𝗛𝗔𝗧𝗦𝗔𝗣𝗣 𝗖𝗛𝗔𝗡𝗡𝗘𝗟👇\n\n>☣️https://chat.whatsapp.com/Lo2XAYfYr3KGV4bo866AXN\n\n>𝗦𝗨𝗕𝗦𝗖𝗥𝗜𝗕𝗘 𝗠𝗬 𝗬𝗢𝗨𝗧𝗨𝗕𝗘 𝗖𝗛𝗔𝗡𝗡𝗘𝗟👇\n\n>☣️https://youtu.be/xSArkTWDXBs?si=447mUzkhuNcjvRYK\n\n>𝗧𝗛𝗔𝗡𝗞 𝗬𝗢𝗨 𝗩𝗘𝗥𝗥𝗬 𝗠𝗨𝗖𝗛 𝗔𝗟𝗟😍❤️",
   MODE: process.env.MODE || "private",
-  API_KEY: process.env.API_KEY || "sky|2483faa7f5630311464123d017fc7acc2aec6da0",
+  MOVIE_API_KEY: process.env.API_KEY || "sky|2483faa7f5630311464123d017fc7acc2aec6da0",
 };
